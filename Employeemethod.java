57a27c1567befab7dcd55dc435bc491ec082b2a9
Arika-Gulati <gulatiarika06@gmail.com>
2020-11-03 22:25:49 +0530

    employee method question added

---
+++ Employeemethod.java
@@ -0,0 +1,34 @@
+public class Employeemethod {
+   public static void method() {
+        int emppresence=(int)Math.floor(Math.random()*10)%3;
+        System.out.println(emppresence);
+        int wageperhr=20;
+        int empwage=0;
+        int time=0;
+switch(emppresence) {
+        case 1:
+                System.out.println("Employee is present for full time");
+                 time=8;
+        break;
+        case 2:
+                System.out.println("Employee is present for part time");
+                 time=4;
+        break;
+
+        default:
+                System.out.println("Employee is absent");
+                time=0;
+  }
+ empwage=wageperhr*time;
+ System.out.println("wage is "+empwage);
+
+   }
+public static void main(String args[]) {
+method();
+   }
+}
+
+
+
+
+
